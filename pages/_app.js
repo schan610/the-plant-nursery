fdc9469666c905477a9a1accc3dae03906bef6b5
schan610 <s.chan610@gmail.com>
2023-03-10 12:38:25 -0800

    save cart to local storage with redux-persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import "../styles/main.scss";
 import Navigation from "../components/Layout/navigation/Navigation";
 import Footer from "../components/Layout/footer/Footer";
-import store from "../store/store";
+import { persistor, store } from "../store/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
-      <Navigation />
-      <main>
-        <div id="modal-root" />
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+        <main>
+          <div id="modal-root" />
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </PersistGate>
     </Provider>
   );
 };
--- store/store.js
+++ store/store.js
@@ -1,7 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartSlice from "./cartSlice";
-export default configureStore({
-  reducer: {
-    cart: cartSlice,
-  },
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartSlice);
+
+export const store = configureStore({
+  reducer: { cart: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
