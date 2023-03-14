ced1a67824249bf64e0a1537b4a2990404d597eb
schan610 <s.chan610@gmail.com>
2023-03-14 13:02:36 -0700

    animated storydetails

--- components/About/OurStory.js
+++ components/About/OurStory.js
@@ -1,6 +1,8 @@
 import { BiChevronsDown, BiChevronsUp } from "react-icons/bi";
 import { useState } from "react";
 import Link from "next/link";
+import StoryDetails from "./StoryDetails";
+import { AnimatePresence } from "framer-motion";
 const OurStory = () => {
   const [showStoryDetails, setShowStoryDetails] = useState(false);
   return (
@@ -32,64 +34,9 @@ const OurStory = () => {
         >
           Read More {showStoryDetails ? <BiChevronsUp /> : <BiChevronsDown />}
         </button>
-        {showStoryDetails && (
-          <section className="story__content">
-            <article className="story__content__item">
-              <div className="story__content__text">
-                <h2 className="heading-secondary">
-                  <span>2018</span> &#8212; Startup
-                </h2>
-                <p>
-                  With years of nursing plants and collecting handfuls of
-                  healthy cuttings, we decided to start a business of our own in
-                  a small studio apartment.
-                </p>
-              </div>
-              <div className="story__content__img">
-                <img
-                  src="https://images.unsplash.com/photo-1449247613801-ab06418e2861?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
-                  alt=""
-                />
-              </div>
-            </article>
-            <article className="story__content__item">
-              <div className="story__content__img">
-                <img
-                  src="https://images.unsplash.com/photo-1607163365613-c281acde5012?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                  alt=""
-                />
-              </div>
-              <div className="story__content__text">
-                <h2 className="heading-secondary">
-                  <span>2020</span> &#8212; Expanding
-                </h2>
-                <p>
-                  2 years after developing a business, sales have been racking
-                  up and the small studio apartment is getting cramped. We
-                  needed to expand.
-                </p>
-              </div>
-            </article>
-            <article className="story__content__item">
-              <div className="story__content__text">
-                <h2 className="heading-secondary">
-                  <span>2022</span> &#8212; Plant Nursery
-                </h2>
-                <p>
-                  Fast-forward to present day, we have successfully expanded to
-                  an outdoor plant nursery where our plants are thriving with a
-                  store where customers visit on a daily basis.
-                </p>
-              </div>
-              <div className="story__content__img">
-                <img
-                  src="https://images.unsplash.com/photo-1678254487407-a28544364f80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1510&q=80"
-                  alt=""
-                />
-              </div>
-            </article>
-          </section>
-        )}
+        <AnimatePresence>
+          {showStoryDetails && <StoryDetails />}
+        </AnimatePresence>
       </div>
     </section>
   );
