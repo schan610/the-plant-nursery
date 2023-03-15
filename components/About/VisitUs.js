7da7b22fa29f7be4698102d08d5b925c67e4ed0d
schan610 <s.chan610@gmail.com>
2023-03-15 16:50:34 -0700

    setup working contact with nodemailer and sendgrid

--- components/About/VisitUs.js
+++ components/About/VisitUs.js
@@ -1,8 +1,40 @@
 import { useRef } from "react";
 const VisitUs = () => {
-  const handleSubmit = (e) => {
+  const contactName = useRef();
+  const contactEmail = useRef();
+  const contactSubject = useRef();
+  const contactMessage = useRef();
+
+  const resetForm = () => {
+    contactName.current.value = "";
+    contactEmail.current.value = "";
+    contactSubject.current.value = "";
+    contactMessage.current.value = "";
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(`submitted form`);
+
+    const data = {
+      name: contactName.current.value,
+      email: contactEmail.current.value,
+      subject: contactSubject.current.value,
+      text: contactMessage.current.value,
+    };
+
+    const response = await fetch("http://localhost:3000/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      return;
+    }
+    resetForm();
   };
   return (
     <section className="visit">
@@ -22,17 +54,18 @@ const VisitUs = () => {
             <h3 className="heading-tertiary">Email us</h3>
             <form action="">
               <label htmlFor="contact-name">Name</label>
-              <input type="text" id="contact-name" />
+              <input type="text" id="contact-name" ref={contactName} />
               <label htmlFor="contact-email">Email</label>
-              <input type="email" id="contact-email" />
+              <input type="email" id="contact-email" ref={contactEmail} />
               <label htmlFor="contact-subject">Subject</label>
-              <input type="text" id="contact-subject" />
+              <input type="text" id="contact-subject" ref={contactSubject} />
               <label htmlFor="contact-message">Message</label>
               <textarea
                 name=""
                 id="contact-message"
                 cols="30"
                 rows="10"
+                ref={contactMessage}
               ></textarea>
 
               <button
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,29 @@
-export default function (req, res) {
-  console.log(req.body);
+const SENDGRID_USER = process.env.SENDGRID_USER;
+const SENDGRID_PASSWORD = process.env.SENDGRID_PASSWORD;
+
+const nodemailer = require("nodemailer");
+export default async function (req, res) {
+  const data = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 465,
+    auth: {
+      user: SENDGRID_USER,
+      pass: SENDGRID_PASSWORD,
+    },
+  });
+
+  await transporter.sendMail({
+    ...data,
+    to: "plantnursery.service@gmail.com",
+    from: "plantnursery.service@gmail.com",
+    html: `<div>
+    <p>Sender name: ${data.name}</p>
+    <p>Sender email: ${data.email}</p>
+    <p>Sender message: ${data.text}</p>
+    </div>`,
+  });
+
+  res.status(200);
 }
